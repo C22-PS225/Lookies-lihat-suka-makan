c0ab75ed9155287677384384498fa19b9c1641e3
immanuelsindu <immanuel.sindu@ti.ukdw.ac.id>
2022-05-29 17:16:04 +0700

    add captureCamera

--- mobile_development/app/src/main/java/com/example/lookies/CameraActivity.kt
+++ mobile_development/app/src/main/java/com/example/lookies/CameraActivity.kt
@@ -1,19 +1,23 @@
 package com.example.lookies
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.lookies.databinding.ActivityCameraAcitivityBinding
 
-private var imageCapture: ImageCapture? = null
-private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-private lateinit var binding: ActivityCameraAcitivityBinding
+
 class CameraActivity : AppCompatActivity() {
+
+    private var imageCapture: ImageCapture? = null
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+    private lateinit var binding: ActivityCameraAcitivityBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +27,44 @@ class CameraActivity : AppCompatActivity() {
         this.supportActionBar?.hide()
         startCamera()
 
+        binding.captureImage.setOnClickListener { takePhoto() }
+        binding.switchCamera.setOnClickListener {
+            cameraSelector = if (cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) CameraSelector.DEFAULT_FRONT_CAMERA
+            else CameraSelector.DEFAULT_BACK_CAMERA
 
+            startCamera()
+        }
 
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createFile(application)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Toast.makeText(
+                        this@CameraActivity,
+                        "Gagal mengambil gambar.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                    )
+                    setResult(PreCameraCapture.CAMERA_X_RESULT, intent)
+                    finish()
+
+                }
+            }
+        )
     }
 
     private fun startCamera() {
@@ -59,5 +99,7 @@ class CameraActivity : AppCompatActivity() {
             }
         }, ContextCompat.getMainExecutor(this))
     }
+
+
 }
 
--- mobile_development/app/src/main/java/com/example/lookies/PreCameraCapture.kt
+++ mobile_development/app/src/main/java/com/example/lookies/PreCameraCapture.kt
@@ -3,15 +3,33 @@ package com.example.lookies
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.lookies.databinding.ActivityPreCameraCaptureBinding
+import java.io.File
 
 class PreCameraCapture : AppCompatActivity() {
     private lateinit var binding: ActivityPreCameraCaptureBinding
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            val result = rotateBitmap(
+                BitmapFactory.decodeFile(myFile.path),
+                isBackCamera
+            )
+
+            binding.previewImageView.setImageBitmap(result)
+        }
+    }
 
     companion object {
         const val CAMERA_X_RESULT = 200
@@ -54,8 +72,12 @@ class PreCameraCapture : AppCompatActivity() {
         }
 
         binding.btnOpenCamera.setOnClickListener {
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
+            startCameraX()
         }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
     }
 }
