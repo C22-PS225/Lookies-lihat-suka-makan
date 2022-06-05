786ef57f3c7fff9f1d4b6ea9b17676fd180e4d64
immanuelsindu <immanuel.sindu@ti.ukdw.ac.id>
2022-06-05 18:13:28 +0700

    tambah navigasi antar fragment

---
+++ mobile_development/app/src/main/java/com/example/lookies/AccountInformation.kt
@@ -0,0 +1,11 @@
+package com.example.lookies
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AccountInformation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_information)
+    }
+}
--- mobile_development/app/src/main/java/com/example/lookies/MainActivity.kt
+++ mobile_development/app/src/main/java/com/example/lookies/MainActivity.kt
@@ -2,15 +2,19 @@ package com.example.lookies
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lookies.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var rcySpecialForYou: RecyclerView
     private lateinit var rcySpecialForYou2: RecyclerView
-//    private lateinit var rcySpecialForYou2: RecyclerView
     private val list = ArrayList<SpecialForYou>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,19 +23,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         this.supportActionBar?.hide()
+        val navView: BottomNavigationView = binding.myBottomNavigationView
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.myHome, R.id.mySearch, R.id.myPlaceholder, R.id.myAccount, R.id.mySetting
+        ).build()
+
+//        val appBarConfiguration = AppBarConfiguration(
+//            setOf(
+//                R.id.myHome, R.id.mySearch, R.id.myPlaceholder, R.id.myAccount, R.id.mySetting
+//            )
+//        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
 //        binding.myBottomNavigationView.background = null
 //        binding.myBottomNavigationView.menu.getItem(2).isEnabled = false
-
-        rcySpecialForYou = findViewById(R.id.rcyViewSpesialForYou)
-        rcySpecialForYou.setHasFixedSize(true)
-        rcySpecialForYou2 = findViewById(R.id.rcyViewSpesialForYou2)
-        rcySpecialForYou.setHasFixedSize(true)
+//
+//        rcySpecialForYou = findViewById(R.id.rcyViewSpesialForYou)
+//        rcySpecialForYou.setHasFixedSize(true)
+//        rcySpecialForYou2 = findViewById(R.id.rcyViewSpesialForYou2)
+//        rcySpecialForYou.setHasFixedSize(true)
 
 //        rcySpecialForYou2 = findViewById(R.id.rcyViewSpesialForYou2)
 //        rcySpecialForYou2.setHasFixedSize(true)
 
-        list.addAll(listSpecial)
-        showRecyclerList()
+//        list.addAll(listSpecial)
+//        showRecyclerList()
 
 //        binding.myFabCamera.setOnClickListener{
 //            var intent = Intent(this, PreCameraCapture::class.java)
@@ -39,31 +60,25 @@ class MainActivity : AppCompatActivity() {
 //        }
     }
 
-    private val listSpecial: ArrayList<SpecialForYou>
-        get() {
-            val dataName = resources.getStringArray(R.array.data_name)
-            val dataDescription = resources.getStringArray(R.array.data_description)
-            val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
-            val listHero = ArrayList<SpecialForYou>()
-            for (i in dataName.indices) {
-                val hero = SpecialForYou(dataName[i],dataDescription[i], dataPhoto.getResourceId(i, -1))
-                listHero.add(hero)
-            }
-            return listHero
-        }
-    private fun showRecyclerList() {
-        rcySpecialForYou.layoutManager = LinearLayoutManager(this)
-        val listHeroAdapter = ListSpecialForYouAdapter(list)
-        rcySpecialForYou.adapter = listHeroAdapter
-
-        rcySpecialForYou2.layoutManager = LinearLayoutManager(this)
-        val listHeroAdapter2 = ListSpecialForYouAdapter(list)
-        rcySpecialForYou2.adapter = listHeroAdapter2
-
+//    private val listSpecial: ArrayList<SpecialForYou>
+//        get() {
+//            val dataName = resources.getStringArray(R.array.data_name)
+//            val dataDescription = resources.getStringArray(R.array.data_description)
+//            val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+//            val listHero = ArrayList<SpecialForYou>()
+//            for (i in dataName.indices) {
+//                val hero = SpecialForYou(dataName[i],dataDescription[i], dataPhoto.getResourceId(i, -1))
+//                listHero.add(hero)
+//            }
+//            return listHero
+//        }
+//    private fun showRecyclerList() {
+//        rcySpecialForYou.layoutManager = LinearLayoutManager(this)
+//        val listHeroAdapter = ListSpecialForYouAdapter(list)
+//        rcySpecialForYou.adapter = listHeroAdapter
+//
 //        rcySpecialForYou2.layoutManager = LinearLayoutManager(this)
 //        val listHeroAdapter2 = ListSpecialForYouAdapter(list)
 //        rcySpecialForYou2.adapter = listHeroAdapter2
-    }
-
-
+//    }
 }
