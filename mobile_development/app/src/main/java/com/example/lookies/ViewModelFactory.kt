6fb72e3c32c5d383f9761b0ab5d9788a62c0581c
Najla Salsabila Pulungan <najlaasalsabilaa@gmail.com>
2022-06-09 14:12:59 +0700

    Menambah sign in view model

--- mobile_development/app/src/main/java/com/example/lookies/ViewModelFactory.kt
+++ mobile_development/app/src/main/java/com/example/lookies/ViewModelFactory.kt
@@ -3,13 +3,16 @@ package com.example.lookies
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class ViewModelFactory :
+class ViewModelFactory(private val pref: UserPreference) :
     ViewModelProvider.NewInstanceFactory() {
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
                 SignUpViewModel() as T
+            }
+            modelClass.isAssignableFrom(SignInViewModel::class.java) -> {
+                SignInViewModel(pref) as T
             }
             else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
         }
