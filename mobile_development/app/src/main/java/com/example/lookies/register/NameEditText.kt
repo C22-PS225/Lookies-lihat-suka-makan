d1a24ce5e0715123c8c52a81d1aa4e1d581a0646
Najla Salsabila Pulungan <najlaasalsabilaa@gmail.com>
2022-06-10 21:46:11 +0700

    Menambah custom view untuk nama

---
+++ mobile_development/app/src/main/java/com/example/lookies/register/NameEditText.kt
@@ -0,0 +1,48 @@
+package com.example.lookies.register
+
+import android.content.Context
+import android.graphics.Canvas
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import com.example.lookies.R
+
+class NameEditText : AppCompatEditText {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+    }
+
+    private fun init() {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable) {
+                if (s.toString().isEmpty())
+                    error = context.getString(R.string.username_error)
+            }
+
+        })
+    }
+}
