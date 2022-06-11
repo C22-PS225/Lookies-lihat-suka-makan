4edacb62eb1b924d885be9b790ae1f115258c63c
Najla Salsabila Pulungan <najlaasalsabilaa@gmail.com>
2022-06-11 16:39:37 +0700

    Perbaikan dan tambahan

--- mobile_development/app/src/main/java/com/example/lookies/favorite/FavoriteCakesActivity.kt
+++ mobile_development/app/src/main/java/com/example/lookies/favorite/FavoriteCakesActivity.kt
@@ -2,11 +2,52 @@ package com.example.lookies.favorite
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.lookies.R
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.lookies.CakesAdapter
+import com.example.lookies.FavCakes
+import com.example.lookies.MainViewModel
+import com.example.lookies.databinding.ActivityFavoriteCakesBinding
 
 class FavoriteCakesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFavoriteCakesBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: CakesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favorite_cakes)
+        binding = ActivityFavoriteCakesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
+        adapter = CakesAdapter()
+        binding.apply {
+            rvFavCakes.layoutManager = LinearLayoutManager(this@FavoriteCakesActivity)
+            rvFavCakes.setHasFixedSize(true)
+            rvFavCakes.adapter = adapter
+        }
+
+        viewModel.getFavCakes().observe(this) {
+            val favCakes = mapUserList(it)
+            if (it != null) {
+                adapter.getData(favCakes)
+            }
+        }
+    }
+
+    private fun mapUserList(it: List<FavCakes>): ArrayList<FavCakes> {
+        val listCakes = ArrayList<FavCakes>()
+        for (cakes in it) {
+            val favUserMapped = FavCakes(
+                cakes.idKue,
+                cakes.gambar,
+                cakes.namaKue,
+                cakes.paragraf1
+            )
+            listCakes.add(favUserMapped)
+        }
+        return listCakes
     }
 }
