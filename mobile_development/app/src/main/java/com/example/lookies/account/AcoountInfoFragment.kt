3ec46f9b51a154e10f22e2c5d12efafecb1ea02e
Najla Salsabila Pulungan <najlaasalsabilaa@gmail.com>
2022-06-12 00:29:39 +0700

    Menambah fitur log out

--- mobile_development/app/src/main/java/com/example/lookies/account/AcoountInfoFragment.kt
+++ mobile_development/app/src/main/java/com/example/lookies/account/AcoountInfoFragment.kt
@@ -1,45 +1,48 @@
 package com.example.lookies.account
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
-import com.example.lookies.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.lookies.UserPreference
+import com.example.lookies.ViewModelFactory
+import com.example.lookies.databinding.FragmentAcoountInfoBinding
+import com.example.lookies.login.SignInActivity
+import com.example.lookies.login.SignInViewModel
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user")
 
 class AcoountInfo : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AcoountInfo().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentAcoountInfoBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: SignInViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_acoount_info, container, false)
+    ): View {
+        _binding = FragmentAcoountInfoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(
+            requireActivity(),
+            ViewModelFactory(UserPreference.getInstance(requireContext().dataStore))
+        )[SignInViewModel::class.java]
+        binding.logoutFab.setOnClickListener {
+            viewModel.userLogout()
+            startActivity(Intent(requireActivity(), SignInActivity::class.java))
+            activity?.finish()
+        }
+    }
 }
